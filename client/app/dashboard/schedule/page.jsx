da36c80b8442430c3a79829654d9c107eb438efb
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-08-16 10:02:39 +0530

    Update page.jsx

--- client/app/dashboard/schedule/page.jsx
+++ client/app/dashboard/schedule/page.jsx
@@ -46,7 +46,7 @@ function page() {
 
   return (
      <div className={`absolute xl:top-15 xl:left-22 2xl:left-25 lg:top-15 lg:left-20 md:top-17 md:left-0 md:w-full md:h-[89vh] sm:top-17 sm:left-0 sm:w-full sm:h-[89vh] h-[89vh] w-full top-17 left-0 border-gray-900 border-1 rounded-2xl xl:h-[89dvh] lg:h-[91vh] 2xl:h-[92vh] bg-[#111111] text-white flex flex-col items-center gap-5 
-    ${boolValue ? "xl:w-[81vw] lg:w-[78vw] 2xl:w-[81vw] md:w-full xl:left-65 2xl:left-74 lg:left-57 md:left-75" : "xl:w-[93vw] 2xl:w-[93vw] lg:w-[92vw]"}`}>
+    ${boolValue ? "xl:w-[81vw] lg:w-[78vw] 2xl:w-[81vw] md:w-full xl:left-71 2xl:left-72 lg:left-63 md:left-80" : "xl:w-[93vw] 2xl:w-[93vw] lg:w-[92vw]"}`}>
       <div className="flex flex-col h-full w-full items-center justify-between">
         <div className="h-16 w-full border-b-gray-900 border-b-1"></div>
         <div className="flex h-full w-full items-center justify-center">
