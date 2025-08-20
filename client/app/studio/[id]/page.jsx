bcb1f9cbed5784d28a50dfc6c24e206134f01cbb
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-08-20 08:03:05 +0530

    Update page.jsx

--- client/app/studio/[id]/page.jsx
+++ client/app/studio/[id]/page.jsx
@@ -347,6 +347,12 @@ function page() {
         pc.addTrack(track, stream);
       });
 
+      const audio = stream?.getAudioTracks()[0];
+
+      if(audio){
+       socket.emit("audio",{ audio : audio.enabled , roomId : param.id});   
+      }
+
       pc.onicecandidate = e => {
        if (e.candidate) {
         socket.emit('signal', { target: id, signal: e.candidate });
@@ -378,7 +384,6 @@ function page() {
     const audioTrack = med?.getAudioTracks()[0];
     if (audioTrack) {
       audioTrack.enabled = !audioTrack.enabled;
-      socket.emit("audio",{ audio : audioTrack.enabled , roomId : param.id});
       setMicOn(audioTrack.enabled);
       setBorder(!audioTrack.enabled);
     }
@@ -623,10 +628,10 @@ function page() {
             RoomId : {param.id}
            </div>
          </div>
-         <div className={`h-[90vh] sm:h-[84vh] md:h-[83vh] lg:h-[80vh] xl:h-[75vh] w-full flex flex-wrap justify-center gap-2 p-3 ${ user <=2 ? "items-center justify-center" : ""}`}>
+         <div className={`h-[90vh] sm:h-[84vh] md:h-[83vh] lg:h-[80vh] xl:h-[75vh] w-full flex flex-wrap justify-center gap-1 sm:gap-2 p-3 ${ user <=2 ? "items-center justify-center" : ""}`}>
           {
               localVideoRef ? (
-               <div className={`relative ${ user === 1 ? "h-full w-full flex justify-center items-center" : ""} ${ user <= 2 ? "h-[35vh] sm:h-[52vh] md:h-[60vh] lg:h-[70vh] xl:h-[64vh] xl:w-[40vw] lg:w-[35vw] sm:w-[45vw] md:w-[35vw] w-full" : "h-[35vh] sm:h-[35vh] md:h-[40vh] xl:h-[35vh] w-[40vw] sm:w-[30vw] md:w-[25vw] xl:w-[22vw]"}`}>
+               <div className={`relative ${ user === 1 ? "h-full w-full flex justify-center items-center" : ""} ${ user <= 2 ? "h-[35vh] sm:h-[52vh] md:h-[60vh] lg:h-[70vh] xl:h-[64vh] xl:w-[40vw] lg:w-[35vw] sm:w-[45vw] md:w-[35vw] w-full" : "h-[35vh] sm:h-[35vh] md:h-[40vh] xl:h-[35vh] w-[44vw] sm:w-[30vw] md:w-[25vw] xl:w-[22vw]"}`}>
                   <video 
                 ref={localVideoRef} 
                 autoPlay 
@@ -643,7 +648,7 @@ function page() {
             peers && remoteStream && (
                Object.entries(peers).map(([id,{ stream , na }]) => (
                  <div className={`rounded-md overflow-hidden relative
-                  ${ user <= 2 ? "h-[35vh] sm:h-[52vh] md:h-[60vh] lg:h-[70vh] xl:h-[64vh] xl:w-[40vw] lg:w-[35vw] sm:w-[45vw] md:w-[35vw] w-full" : "h-[35vh] sm:h-[35vh] md:h-[38vh] xl:h-[35vh] w-[40vw] sm:w-[30vw] md:w-[24vw] xl:w-[22vw]"}`}>
+                  ${ user <= 2 ? "h-[35vh] sm:h-[52vh] md:h-[60vh] lg:h-[70vh] xl:h-[64vh] xl:w-[40vw] lg:w-[35vw] sm:w-[45vw] md:w-[35vw] w-full" : "h-[35vh] sm:h-[35vh] md:h-[38vh] xl:h-[35vh] w-[44vw] sm:w-[30vw] md:w-[24vw] xl:w-[22vw]"}`}>
                   <video key={id} autoPlay playsInline ref={video => video && (video.srcObject = stream)} 
                   className={`rounded-md h-full w-full object-cover ${partnerVoice ? "" : "border-2 border-green-600"}`} />
                 <div className={`absolute bottom-1 font-bold left-1 text-white ${ stream ? "block" : "hidden"}`}>{na}</div>
